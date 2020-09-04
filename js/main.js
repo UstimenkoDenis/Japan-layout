5c4aa41c7b772d4752e3eeb93c59ebd48df7ebd3
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-09-05 00:21:30 +0300

    header adaptive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-(function(){
-   const header = document.querySelector('.header')
+(function () {
+   const header = document.querySelector('.header')        
+
   window.onscroll = () => {
       if(window.pageYOffset > 50) {
         header.classList.add('header-active')
       } else {
         header.classList.remove('header-active')}
-      }
-  })()
+      } 
+})()
+
+// Burger Handler
+
+(function () {
+  const menu = document.querySelector('.header__nav'),
+        burgerItem  = document.querySelector('.burger'),
+        menuCloseItem = document.querySelector('.header__nav-close')
+
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header__nav_active')
+  })
+
+  menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('header__nav_active')
+  })
+
+}())
